3585e7619fee06002c3b244535e3509fde058ded
Forrest <Forrestmillerj@gmail.com>
2020-04-08 20:12:28 -0500

    added comments

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const logger = require("morgan");
+// define port
 const PORT = process.env.PORT || 3000;
-const path = require('path');
-
+// create express app
 const app = express();
+// set express to be able to use json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// serve public files
 app.use(express.static('public'));
-
+// set up mongoose database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutsdb', {useNewUrlParser: true});
 
 // require routes
 require('./routes/api-routes.js')(app);
 require('./routes/html-routes.js')(app);
 
+// start app
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
