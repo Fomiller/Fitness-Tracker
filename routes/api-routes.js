6e0064fb968bd4e5097f63a094f5d4d018d309b6
Forrest <Forrestmillerj@gmail.com>
2020-04-09 18:21:30 -0500

    Now adding exercises to a workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,7 +5,6 @@ module.exports = function(app){
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
       .then(dbWorkout => {
-        console.log(dbWorkout);
         res.json(dbWorkout);
       })
       .catch(err => {
@@ -13,7 +12,7 @@ module.exports = function(app){
       });
   });
 
-// creates a WORKOUT
+  // creates a WORKOUT
   app.post('/api/workouts', ({ body }, res) => {
     // create a new exercise using request.body
     const workout = new db.Workout(body);
@@ -29,13 +28,16 @@ module.exports = function(app){
       });
   });
 
-  // pushes an EXERCISE into a WORKOUT
+  // add/pushes an EXERCISE into a WORKOUT
+  // Updates the Workout Model
   app.put('/api/workouts/:id', (req, res) => {
     // create a new exercise using request.body
+    const workoutID = req.params.id
     const exercise = new db.Exercise(req.body);
+    console.log('EXERCISE TO ADD: ',exercise);
 
     db.Exercise.create(exercise)
-      .then(({_id}) => db.Workout.findOneAndUpdate({_id: req.params.id}, {$push: {exercises: _id}}))
+      .then(({ _id }) => db.Workout.findOneAndUpdate({ _id: workoutID }, { $push: { exercises: _id }}, { new: true }))
       .then(dbWorkout => {
         res.json(dbWorkout);
       })
@@ -44,4 +46,25 @@ module.exports = function(app){
         res.json(err);
       });
   });
-}
+
+  app.get('/api/workouts/:id', (req, res) => {
+    db.Workout.findOne({ _id: req.params.id })
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
+
+  app.get('/populateworkout', (req, res) => {
+    db.Workout.find({})
+    .populate('exercises')
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
+};
