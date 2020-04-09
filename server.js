99e0b34976eb6b6379110ec5086def667d46462f
Forrest <Forrestmillerj@gmail.com>
2020-04-09 16:45:50 -0500

    Updated

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const logger = require("morgan");
+
 // define port
 const PORT = process.env.PORT || 3000;
+
 // create express app
 const app = express();
+
+// require models
+
+// set up morgan
+app.use(logger('dev'))
+
 // set express to be able to use json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // serve public files
 app.use(express.static('public'));
+
 // set up mongoose database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {useNewUrlParser: true});
 
